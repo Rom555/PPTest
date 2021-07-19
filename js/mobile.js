1bc24de6697aba980c0d16743b4061c51fe41a6b
Rom555 <romanzubrilin@gmail.com>
2021-07-19 09:35:25 +0300

    resize fix

--- js/mobile.js
+++ js/mobile.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
+  mobile = false; 
   function windowSize(){
-    if ($(window).width() <= 400){
+    if ($(window).width() <= 400 && !mobile){
       $(".check-form").appendTo(".check-tabs__item--active");
       $(".hero__img").appendTo(".hero")
+      mobile = true
+    } else {
+      $(".check-form").appendTo(".check")
+      $(".hero__img").appendTo(".hero-wrapper")
+      mobile = false
     }
-    
   }
 
   $(window).on('load resize',windowSize);
